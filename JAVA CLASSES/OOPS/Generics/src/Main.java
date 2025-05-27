9d6e8c9956f33c513f3f5183c5aca5708de48fe4
Ihaxxxx <mubashir.mandvia@gmail.com>
2025-05-27 23:24:34 +0500

    Generics done

--- JAVA CLASSES/OOPS/Generics/src/Main.java
+++ JAVA CLASSES/OOPS/Generics/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        Box<Integer> box = new Box<>();
+
+        box.setItem(10);
+
+        System.out.println(box.getItem());
+
+        Product<String,Double> product1 = new Product<>("Pizza",5.5);
+        Product<String,Integer> product2 = new Product<>("Pizza",15);
+
+        System.out.println(product1.getItem());
+        System.out.println(product2.getPrice());
+
 
     }
 }
